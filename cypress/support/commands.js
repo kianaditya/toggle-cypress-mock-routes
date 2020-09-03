1f84152db04f897c34a18f27defbcb64799fc5d7
Aditya <naik.aditya@gmail.com>
2020-09-03 15:05:19 +0200

    minor fix

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,32 +1,6 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add("login", (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
-
 Cypress.Commands.add('customRoute', (args, alias = 'mock') => {
+  cy.server()
   if (Cypress.env('mock')) {
-    cy.server()
     cy.route(args).as(alias)
   }
 })
