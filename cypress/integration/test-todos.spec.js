f534f1a801a028b2d97a65b7c86907cef6624069
Aditya <naik.aditya@gmail.com>
2020-09-03 11:20:54 +0200

    Sets up fixture, customRoute and toggle

--- cypress/integration/test-todos.spec.js
+++ cypress/integration/test-todos.spec.js
@@ -2,10 +2,22 @@
 
 describe('Tests todo list', () => {
   it('Checks todo render', () => {
+    cy.customRoute(
+      {
+        method: 'GET',
+        url: 'https://jsonplaceholder.typicode.com/todos',
+        response: 'fixture:todo.json',
+      },
+      'getTodos'
+    )
     cy.visit('/')
+    // You can check env variable status by console.log(Cypress.env('mock'))
     cy.get('h3')
-      .should('have.length', 200)
+      .should('have.length', Cypress.env('mock') ? 1 : 200)
       .first()
-      .should('have.text', 'delectus aut autem')
+      .should(
+        'have.text',
+        Cypress.env('mock') ? 'Mocked Todo' : 'delectus aut autem'
+      )
   })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,10 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('customRoute', (args, alias = 'mock') => {
+  if (Cypress.env('mock')) {
+    cy.server()
+    cy.route(args).as(alias)
+  }
+})
